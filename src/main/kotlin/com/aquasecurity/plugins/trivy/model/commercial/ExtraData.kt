5cb02808d80d4ed6aec61cfb5139007caaa0a6b4
Owen Rumney <owen.rumney@aquasec.com>
2025-08-14 10:16:18 +0100

    fix: handle null layer on package when deserializing results (#57)

--- src/main/kotlin/com/aquasecurity/plugins/trivy/model/commercial/ExtraData.kt
+++ src/main/kotlin/com/aquasecurity/plugins/trivy/model/commercial/ExtraData.kt
@@ -3,7 +3,7 @@ package com.aquasecurity.plugins.trivy.model.commercial
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ExtraData(
-    @JsonProperty("References") val references: List<String>,
+    @JsonProperty("References") val references: List<String>?,
     @JsonProperty("Direct") val direct: String?,
     @JsonProperty("PackageRoots") val packageRoots: List<String>?,
     @JsonProperty("Fix") val fix: Fix?,
